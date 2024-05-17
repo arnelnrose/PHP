33efb37b3c02b32532fe66be1f109be4b00eae3d
Esor <155610717+arnelnrose@users.noreply.github.com>
2024-05-17 12:25:07 +0800

    Add files via upload

--- phplearning/07-LogicalOperators.php
+++ phplearning/07-LogicalOperators.php
@@ -1,35 +1,77 @@
 <?php
+/*
+        Logical Operator - combine conditional statements
+        if(conditiona1 && condition2) 
+
+        && = True if both conditions are true.
+        || = True if at least one condition is true.
+        !  = True if flase. False is true.
+    */
+/* EXAMPLE #1
+$temp = 31;
+
+if ($temp >= 00 && $temp <= 30) {
+    echo "The weather is good.";
+} else {
+    echo "The weather is bad.";
+}
+*/
+
+/* EXAMPLE #2
+$temp = 25;
+if ($temp < 0 || $temp > 30) {
+    echo "The weather is bad.";
+} else {
+    echo "The weather is good.";
+};
+*/
+
+/* EXAMPLE #3
+$temp = 25;
+$cloudy = false;
+
+if ($temp < 0 || $temp > 30) {
+    echo "The weather is bad. <br/>";
+} else {
+    echo "The weather is good. <br/>";
+}
+
+if (!$cloudy) {
+    echo "It's sunny. <br/>";
+} else {
+    echo "It's cloudy. <br/>";
+}
+*/
+
+/* EXAMPLE #4
+$age = 17;
+$citizen = true;
+if ($age >= 18 && $citizen) {
+    echo " You can vote";
+} else {
+    echo "You cannot vote";
+}
+*/
+
+/* EXAMPLE #5
+$age = 18;
+$citizen = true;
+if (!$age >= 18 && !$citizen) {
+    echo " You cannot vote";
+} else {
+    echo "You can vote";
+}
+*/
+
+// EXAMPLE #4
 $child = true;
-$senior = ftrue;
+$senior = true;
 $ticket = null;
 
-if ($child || $senior) {
+if (@$child || !$senior) {
     $ticket = 10;
 } else {
     $ticket = 15;
 }
-echo "The ticket price is {$ticket}";
 
-// EXAMPLE #1
-// $age = 25;
-// $citizen = true;
-
-// if ($age >= 18 || !$citizen) {
-//     echo "You cannot vote.";
-// } else {
-//     echo "You can vote.";
-// }
-
-
-// EXAMPLE #2
-// $temp = 35;
-// $cloudy = true;
-
-// if ($temp < 0 || $temp > 30) {
-//     echo "Today's weather condition 
-//     is too hot. <br>";
-// } else {
-//     echo "The weather is good.";
-// }
-// echo $cloudy ? "It's sunny." :
-//     "It's cloudy.";
+echo "The ticket price is \${$ticket}";
