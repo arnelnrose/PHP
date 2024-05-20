3c383ef7ed5cc8584897db47ecb5f89207b8167f
Esor <155610717+arnelnrose@users.noreply.github.com>
2024-05-20 15:31:00 +0800

    Update 21B-Home_Session.php

--- phplearning/21B-Home_Session.php
+++ phplearning/21B-Home_Session.php
@@ -10,7 +10,7 @@ session_start();
 
 <body>
     This is the home page.<br />
-    <!-- <a href="./21A-Index_Session.php">This goes to login page.</a><br /> -->
+    <!-- <a href="./21A-Login_Session.php">This goes to login page.</a><br /> -->
 
     <form action="./21B-Home_Session.php" method="post">
         <input type="submit" name="logout" value="Logout">
@@ -26,6 +26,6 @@ echo "{$_SESSION["password"]} <br/>";
 
 if (isset($_POST["logout"])) {
     session_destroy();
-    header("Location: 21A-Index_Session.php");
+    header("Location: 21A-Login_Session.php");
 }
 ?>
